bee11f9293d4e05ae2a64b6bb52f5a89bd3a69cc
Minho Yu <ymh0770@gmail.com>
2022-02-23 16:38:04 +0900

    add isort

--- pythonProject8/settings.py
+++ pythonProject8/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-from typing import List
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
